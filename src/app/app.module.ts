93d58cfe6ea8714b2c7c33232d699a7a73b5effc
Dario <dario_gow3@hotmail.com>
2024-04-29 03:23:32 -0600

    Home page and button example component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/common/button/button.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
