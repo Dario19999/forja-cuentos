745fc6e04f06941634937e2eb53e4e6a5e5ba0d9
Dario <dario_gow3@hotmail.com>
2025-03-07 19:18:10 -0600

    change character list button colors and appearance logic

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -40,7 +40,7 @@ export class MainMenuComponent {
                     },
                     {
                         title: 'Logout',
-                        theme: 'text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2',
+                        theme: 'text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2',
                         redirectTo: '/login',
                     }
                 ]
